898e3f2a407510416e4c2a54f500c2979108a330
Anna Gutowska <annaggg0@gmail.com>
2023-06-24 14:39:02 -0400

    Add files via upload

---
+++ ERGM.R
@@ -0,0 +1,42 @@
+install.packages('ergm')
+library(ergm)
+install.packages('statnet')
+library(statnet)
+
+#View(nodes_ergm)
+#View(links_ergm)
+
+#remove unnecessary automated column 
+nodes_ergm$X <- NULL
+head(nodes_ergm)
+
+links_ergm$X <- NULL
+head(links_ergm)
+
+snet <- network(links_ergm, vertex.attr = nodes_ergm, matrix.type = "edgelist", loops=F, multiple=F, ignore.eval = F)
+snet
+
+
+# A simple model that includes just the edge (density) parameter:
+#1 
+lap_model <- ergm(snet ~ edges)
+summary(lap_model)
+
+#2
+lap_model <- ergm(snet ~ edges + mutual)  
+summary(lap_model)
+
+#3 using all of the features
+lap_model <- ergm(snet ~ edges #connections 
+                  + mutual #reciprocity 
+                  + gwesp(0.2, fixed=T) #Transitive closure 
+                  + nodeicov("verified.type") #incoming links for verification status
+                  + nodeocov("verified.type") #outgoing links for verification status
+                  + nodecov("X2") #adding topic weights as nodal covariates (used for continuous variables)
+                  + nodecov("X4")
+                  + nodecov("X13")
+                  + nodecov("X26")
+                  + nodecov("X54")
+)
+
+summary(lap_model)
