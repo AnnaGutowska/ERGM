0638286f850169288ebff837589f80939e4ae0c5
Anna Gutowska <annaggg0@gmail.com>
2023-06-29 13:34:35 -0400

    Update ERGM.R

--- ERGM.R
+++ ERGM.R
@@ -8,6 +8,7 @@ library(statnet)
 
 #remove unnecessary automated column 
 nodes_ergm$X <- NULL
+nodes_ergm$verified.type <- NULL
 head(nodes_ergm)
 
 links_ergm$X <- NULL
@@ -27,19 +28,37 @@ lap_model <- ergm(snet ~ edges + mutual)
 summary(lap_model)
 
 #3 using all of the features
-lap_model <- ergm(snet ~ edges #connections 
-                  + mutual #reciprocity 
-                  + gwesp(0.2, fixed=T) #Transitive closure 
-                  + nodeicov("verified.type") #incoming links for verification status
-                  + nodeocov("verified.type") #outgoing links for verification status
-                  + nodecov("X2") #adding topic weights as nodal covariates (used for continuous variables)
-                  + nodecov("X4")
-                  + nodecov("X13")
-                  + nodecov("X26")
-                  + nodecov("X54")#,
-                  #control=control.ergm(MCMLE.maxit= 25)
+lap_model <- ergm(snet ~ nodeofactor("verified.label") #incoming links for verification status
+                  + nodeifactor("verified.label") #outgoing links for verification status
+                  + nodeocov("X2") #adding topic weights as nodal covariates (used for continuous variables)
+                  + nodeicov("X2")
+                  + nodeocov("X4")
+                  + nodeicov("X4")
+                  + nodeocov("X13")
+                  + nodeicov("X13")
+                  + nodeocov("X26")
+                  + nodeicov("X26")
+                  + nodeocov("X54")
+                  + nodeicov("X54")
+                  + mutual
+                  + edges
 )
 
+
+# lap_model <- ergm(snet ~ edges #connections 
+#                   + mutual #reciprocity 
+#                   + gwesp(0.2, fixed=T) #Transitive closure 
+#                   + nodeicov("verified.type") #incoming links for verification status
+#                   + nodeocov("verified.type") #outgoing links for verification status
+#                   + nodecov("X2") #adding topic weights as nodal covariates (used for continuous variables)
+#                   + nodecov("X4")
+#                   + nodecov("X13")
+#                   + nodecov("X26")
+#                   + nodecov("X54")#,
+#                   #control=control.ergm(MCMLE.maxit= 25)
+# )
+
+save(lap_model, file="Model.Rdata")
 summary(lap_model)
 
 #Finding the goodness of fit of this model
